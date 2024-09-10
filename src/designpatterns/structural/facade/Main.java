b026c605382fa9ff410b7bba31a2d86e4eb8eb6d
rahulgyawali <rgyawal1@asu.edu>
2024-09-10 01:44:14 -0700

    Facade Design Pattern

---
+++ src/designpatterns/structural/facade/Main.java
@@ -0,0 +1,13 @@
+package designpatterns.structural.facade;
+
+/**
+ * Used when we have to hide system complexity from the client
+ */
+
+/**
+ * Example 1: We have a Dao and want to only expose certain methods like insert() and update() in contrast to 100s of other methods
+ *
+ * It is different from proxy in the sense that It can take care of multiple classes
+ */
+public class Main {
+}
