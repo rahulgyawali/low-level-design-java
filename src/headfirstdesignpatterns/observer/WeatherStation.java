2851b5636f6d1113754342ee3c7fe3ce03ede423
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2025-04-27 11:13:32 -0700

    Observer Pattern change

--- src/headfirstdesignpatterns/observer/WeatherStation.java
+++ src/headfirstdesignpatterns/observer/WeatherStation.java
@@ -2,9 +2,9 @@ package headfirstdesignpatterns.observer;
 
 public class WeatherStation {
     public static void main(String[] args) {
-        WeatherData weatherData = new WeatherData();
-        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
-        ForecastConditionDisplay forecastConditionDisplay = new ForecastConditionDisplay(weatherData);
+        WeatherData weatherData = new WeatherData();    //Observable
+        Observer currentConditionDisplay = new CurrentConditionDisplay(weatherData);    //Observer
+        Observer forecastConditionDisplay = new ForecastConditionDisplay(weatherData);  //Observer
         weatherData.setData(10,20,30);
         weatherData.setData(100,200,300);
         weatherData.removeObserver(currentConditionDisplay);
