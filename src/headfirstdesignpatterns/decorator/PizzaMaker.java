5d8782f2de01bedb3ea12cdf2782ad9905dd02ce
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2025-04-29 16:42:54 -0700

    Decorator Pattern udpated

--- src/headfirstdesignpatterns/decorator/PizzaMaker.java
+++ src/headfirstdesignpatterns/decorator/PizzaMaker.java
@@ -2,13 +2,13 @@ package headfirstdesignpatterns.decorator;
 
 public class PizzaMaker {
     public static void main(String[] args) {
-        BasePizza basePizza = new MargaritaPizza();
-        System.out.println("Total final cost "+ basePizza.cost());
-        ExtraCheese extraCheese = new ExtraCheese(basePizza);
-        System.out.println("Total final cost "+extraCheese.cost());
-        ExtraOlives extraOlives = new ExtraOlives(extraCheese);
-        System.out.println("Total final cost "+extraOlives.cost());
-        ExtraSauce extraSauce = new ExtraSauce(extraOlives);
-        System.out.println("Total final cost "+extraSauce.cost());
+        BasePizza pizza = new MargaritaPizza();
+        System.out.println("Total final cost "+ pizza.cost());
+        pizza = new ExtraCheese(pizza);
+        System.out.println("Total final cost "+pizza.cost());
+        pizza = new ExtraOlives(pizza);
+        System.out.println("Total final cost "+pizza.cost());
+        pizza = new ExtraSauce(pizza);
+        System.out.println("Total final cost "+pizza.cost());
     }
 }
